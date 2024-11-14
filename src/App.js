11714670d5fe7c4d051d2ea4c59b7f9a297ed510
diazYohary <97261095+diazYohary@users.noreply.github.com>
2024-11-13 21:52:12 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,14 +10,17 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfVcNvVtS0WONfW7v0x1gaHB2px9viFJo",
-  authDomain: "realtimechat-33c56.firebaseapp.com",
-  projectId: "realtimechat-33c56",
-  storageBucket: "realtimechat-33c56.firebasestorage.app",
-  messagingSenderId: "36145972459",
-  appId: "1:36145972459:web:31c6b23037caa5f92e6aae",
-  measurementId: "G-DMCLZ9F0DK"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
+
+export default firebaseConfig;
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
